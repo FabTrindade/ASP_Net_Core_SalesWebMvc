a8f147260a1428a3def1fab2b2c2bbc395110145
FabTrindade <fabrtrindade@gmail.com>
2022-02-20 21:54:18 -0300

    Implementing simple search.

--- SalesWebMvc/Models/SalesRecord.cs
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using SalesWebMvc.Models.Enums;
+using System.ComponentModel.DataAnnotations;
 using SalesWebMvc.Models;
 
 namespace SalesWebMvc.Models
@@ -7,6 +8,7 @@ namespace SalesWebMvc.Models
     public class SalesRecord
     {
         public int Id { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
         public double Amount { get; set; }
         public SaleStatus Status { get; set; }
